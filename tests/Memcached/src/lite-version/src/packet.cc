1a4bbe05ab5eb7c753e2538b92e84936948c67dc
Maizhe Zhang <maizhezh@ring4.eecs.umich.edu>
2024-05-30 17:48:33 -0400

    fix(Memcached): resize issue

--- tests/Memcached/src/lite-version/src/packet.cc
+++ tests/Memcached/src/lite-version/src/packet.cc
@@ -81,7 +81,7 @@ void ParsedPacket::ToBuffers(std::vector<uint8_t> &buffers) {
     AppendBuffer(buffers, buffer.get());
     return;
   }
-  buffers.reserve(buffers.size() + 24 + (extra ? extra->size() : 0) +
+  buffers.reserve(buffers.size() + 24 + (extra ? header.extras_length : 0) +
                   (key ? key->size() : 0) + (value ? value->size() : 0));
   ReverseAppendBuffer(buffers, &header.magic, sizeof(header.magic));
   ReverseAppendBuffer(buffers, &header.opcode, sizeof(header.opcode));
@@ -95,7 +95,8 @@ void ParsedPacket::ToBuffers(std::vector<uint8_t> &buffers) {
   ReverseAppendBuffer(buffers, &header.opaque, sizeof(header.opaque));
   ReverseAppendBuffer(buffers, &header.CAS, sizeof(header.CAS));
   AppendBuffer(buffers, extra.get());
-  buffers.resize(buffers.size() + header.extras_length - extra->size());
+  if (extra)
+    buffers.resize(buffers.size() + header.extras_length - extra->size());
   AppendBuffer(buffers, key.get());
   AppendBuffer(buffers, value.get());
 
