0f730e14659023401c23b50f75385c9b13479716
Yi Chen <yichencs@umich.edu>
2024-07-02 15:38:37 -0400

    fix(LiteMySQL): can't handle the case when a header is distributed in several packets

--- tests/MySQL/src/lite-version/include/packet.hpp
+++ tests/MySQL/src/lite-version/include/packet.hpp
@@ -8,9 +8,12 @@ class Packet {
   using InputIterator = uint8_t *;
 
   enum State {
-    kReadingHeader,
+    kReadingHeader0,
+    kReadingHeader1,
+    kReadingHeader2,
+    kReadingHeader3,
     kReadingPayload,
-  } parsing_state_ = kReadingHeader;
+  } parsing_state_ = kReadingHeader0;
 
  public:
   uint32_t payload_length_ = 0;
@@ -21,18 +24,33 @@ class Packet {
   lite::DeserializeResult Deserialize(InputIterator &begin, InputIterator end) {
     while (begin != end) {
       switch (parsing_state_) {
-        case kReadingHeader:
-          payload_length_ = begin[0] + begin[1] * 256 + begin[2] * 256 * 256;
-          sequence_id_ = begin[3];
-          buffer = std::make_shared<std::vector<uint8_t>>(begin, begin + 4);
-          begin += 4;
+        case kReadingHeader0:
+          buffer = std::make_shared<std::vector<uint8_t>>(begin, begin + 1);
+          payload_length_ = *begin++;
+          parsing_state_ = kReadingHeader1;
+          break;
+        case kReadingHeader1:
+          buffer->push_back(*begin);
+          payload_length_ += *begin++ << 8;
+          parsing_state_ = kReadingHeader2;
+          break;
+        case kReadingHeader2:
+          buffer->push_back(*begin);
+          payload_length_ += *begin++ << 16;
+          parsing_state_ = kReadingHeader3;
+          break;
+        case kReadingHeader3:
+          buffer->push_back(*begin);
+          sequence_id_ = *begin++;
+          parsing_state_ = kReadingPayload;
+          break;
         case kReadingPayload:
           size_t remaining_payload_length =
               payload_length_ - (buffer->size() - 4);
           if (end - begin < remaining_payload_length) {
             buffer->insert(buffer->end(), begin, end);
             begin = end;
-            return lite::DeserializeResult::kIndeterminate;
+            break;
           }
           buffer->insert(buffer->end(), begin,
                          begin + remaining_payload_length);
@@ -43,5 +61,7 @@ class Packet {
     return lite::DeserializeResult::kIndeterminate;
   }
 
-  std::shared_ptr<std::vector<uint8_t>> Serialize() const { return buffer; }
+  std::shared_ptr<std::vector<uint8_t>> Serialize() const {
+    return buffer;
+  }
 };
