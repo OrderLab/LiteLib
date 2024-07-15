d731ae19592989ae8e1e74482333309f8b86322e
Yi Chen <yichencs@umich.edu>
2024-07-14 23:13:24 -0400

    feat(sql-parser): print to any ostream

--- tests/MySQL/src/lite-version/lib/sql-parser/src/util/sqlhelper.h
+++ tests/MySQL/src/lite-version/lib/sql-parser/src/util/sqlhelper.h
@@ -3,37 +3,39 @@
 
 #include "../sql/statements.h"
 
+#include <iostream>
+
 namespace hsql {
 
 // Prints a summary of the given SQLStatement.
-void printStatementInfo(const SQLStatement* stmt);
+void printStatementInfo(std::ostream& os, const SQLStatement* stmt);
 
 // Prints a summary of the given SelectStatement with the given indentation.
-void printSelectStatementInfo(const SelectStatement* stmt, uintmax_t num_indent);
+void printSelectStatementInfo(std::ostream& os, const SelectStatement* stmt, uintmax_t num_indent);
 
 // Prints a summary of the given ImportStatement with the given indentation.
-void printImportStatementInfo(const ImportStatement* stmt, uintmax_t num_indent);
+void printImportStatementInfo(std::ostream& os, const ImportStatement* stmt, uintmax_t num_indent);
 
 // Prints a summary of the given CopyStatement with the given indentation.
-void printExportStatementInfo(const ExportStatement* stmt, uintmax_t num_indent);
+void printExportStatementInfo(std::ostream& os, const ExportStatement* stmt, uintmax_t num_indent);
 
 // Prints a summary of the given InsertStatement with the given indentation.
-void printInsertStatementInfo(const InsertStatement* stmt, uintmax_t num_indent);
+void printInsertStatementInfo(std::ostream& os, const InsertStatement* stmt, uintmax_t num_indent);
 
 // Prints a summary of the given CreateStatement with the given indentation.
-void printCreateStatementInfo(const CreateStatement* stmt, uintmax_t num_indent);
+void printCreateStatementInfo(std::ostream& os, const CreateStatement* stmt, uintmax_t num_indent);
 
 // Prints a summary of the given TransactionStatement with the given indentation.
-void printTransactionStatementInfo(const TransactionStatement* stmt, uintmax_t num_indent);
+void printTransactionStatementInfo(std::ostream& os, const TransactionStatement* stmt, uintmax_t num_indent);
 
 // Prints a summary of the given Expression with the given indentation.
-void printExpression(Expr* expr, uintmax_t num_indent);
+void printExpression(std::ostream& os, Expr* expr, uintmax_t num_indent);
 
 // Prints an ORDER BY clause
-void printOrderBy(const std::vector<OrderDescription*>* expr, uintmax_t num_indent);
+void printOrderBy(std::ostream& os, const std::vector<OrderDescription*>* expr, uintmax_t num_indent);
 
 // Prints WindowDescription.
-void printWindowDescription(WindowDescription* window_description, uintmax_t num_indent);
+void printWindowDescription(std::ostream& os, WindowDescription* window_description, uintmax_t num_indent);
 
 }  // namespace hsql
 
