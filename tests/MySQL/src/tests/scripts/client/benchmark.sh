361c1793cf49ced12dfac3b12217f1ac2ebd8bd3
Yi Chen <yichencs@umich.edu>
2024-08-19 01:38:09 -0400

    feat(mysql-exp): revert sysbench to 1.0.18

--- tests/MySQL/src/tests/scripts/client/benchmark.sh
+++ tests/MySQL/src/tests/scripts/client/benchmark.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
 sysbench \
-/usr/local/share/sysbench/oltp_read_write.lua \
+/usr/share/sysbench/oltp_read_write.lua \
 --db-driver=mysql \
---tables=2 \
+--tables=1 \
 --table-size=1000000 \
 --threads=1 \
---mysql-host=127.0.0.1 \
+--mysql-host=mysql-server \
 --mysql-port=60000 \
 --mysql-user=sbtest \
 --mysql-password=password \
 prepare
 
 sysbench \
-/usr/local/share/sysbench/oltp_read_write.lua \
+/usr/share/sysbench/oltp_read_write.lua \
 --db-driver=mysql \
---report-interval=2 \
---tables=2 \
+--report-interval=1 \
+--tables=1 \
 --table-size=1000000 \
---threads=64 \
+--threads=1 \
 --time=10 \
---mysql-host=127.0.0.1 \
---mysql-port=60000 \
+--mysql-host=mysql-server \
+--mysql-port=59999 \
 --mysql-user=sbtest \
 --mysql-password=password \
 --db-ps-mode=disable \
 --mysql-ignore-errors=2013,1062 \
 --skip_trx=on \
---rand-type=zipfian \
 run
--- tests/MySQL/src/tests/scripts/client/entrypoint.sh
+++ tests/MySQL/src/tests/scripts/client/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-apt update
-apt -y install git
-apt -y install make automake libtool pkg-config libaio-dev
-apt -y install default-libmysqlclient-dev libssl-dev
+# apt update
+# apt -y install git
+# apt -y install make automake libtool pkg-config libaio-dev
+# apt -y install default-libmysqlclient-dev libssl-dev
 
-git clone https://github.com/akopytov/sysbench.git
-cd sysbench
-git checkout de18a036cc65196b1a4966d305f33db3d8fa6f8e
-git apply /workspace/tests/MySQL/src/tests/scripts/client/sysbench.patch
+# git clone https://github.com/akopytov/sysbench.git
+# cd sysbench
+# git checkout 805825fa81f633a7477f15ecdc152441e4ef4c83
+# git apply /workspace/tests/MySQL/src/tests/scripts/client/sysbench.patch
 
-./autogen.sh
-./configure
-make -j
-make install
+# ./autogen.sh
+# ./configure
+# make -j
+# make install
+
+cp /workspace/tests/MySQL/src/tests/scripts/client/oltp_common.lua /usr/share/sysbench/
 
 tail -f /dev/null
