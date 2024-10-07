fe03945d8277c79946c62bb8f5be9fbf7cb398b4
Yi Chen <yichencs@umich.edu>
2024-10-07 00:12:34 -0400

    fix(leveldb-exp): server uses lite-sys image

--- tests/LevelDB/src/tests/scripts/leveldb/entrypoint.sh
+++ tests/LevelDB/src/tests/scripts/leveldb/entrypoint.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
+
+apt-get update
+
+apt-get install -y \
+    libsnappy-dev \
+    libev-dev \
+    libgmp-dev \
+    cpanminus \
+    perl \
+    procps \
+    openssh-client \
+    openssh-server \
+    python3 \
+    python3-pip \
+    tar \
+    cmake \
+    htop \
+    iotop \
+    iftop \
+    tcpdump
+
+cpanm --quiet --notest --skip-satisfied --force Redis
+
 cp /workspace/scripts/.ssh ~ -r
 chmod 600 ~/.ssh/id_ed25519
 service ssh start
