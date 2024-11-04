7dbb697344f84a3572aea26a5266fc3a64313377
Yi Chen <yichencs@umich.edu>
2024-11-03 18:48:15 -0600

    feat(Memcached-exp): setup_client for Cloudlab

--- tests/Memcached/src/Metastability/setup_scripts/setup_client.sh
+++ tests/Memcached/src/Metastability/setup_scripts/setup_client.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
-apt update
-apt -y install ssh
-service ssh start
-cp /workspace/.ssh /root -r
-chmod 600 /root/.ssh/id_ed25519
-apt -y install libjpeg8-dev zlib1g-dev curl
-NGINX_SERVER_IP=${1:-"web"}
-MEMCACHED_SERVER_IP=${2:-"memcached"}
-DB_ENTRIES=${3:-"140000"}
-MASTER_VM=${4:-"client"}
-apt-get update;
-apt -y install build-essential;
-apt-get -y install python3-pip;
-pip3 install matplotlib
-pip3 install scipy
-pip3 install pymemcache
-pip3 install SciencePlots
-apt-get install sshpass
-sed -i "/string ngnix_server_ip =/c\string ngnix_server_ip =\"$NGINX_SERVER_IP\";" ../LoadGenerator/TraceReplay.cpp
-sed -i "/memcached_host =/c\memcached_host = \'$MEMCACHED_SERVER_IP\'" ../LoadGenerator/run_experiment.py 
-sed -i "/master_vm =/c\master_vm = \'$MASTER_VM\'" ../LoadGenerator/run_experiment.py 
 
+set -e
+set -x
+
+NGINX_SERVER_IP=${1:-"node0"}
+MEMCACHED_SERVER_IP=${2:-"node1"}
+DB_ENTRIES=${3:-"140000"}
+MASTER_VM=${4:-"node3"}
+
+sudo apt -y install libjpeg8-dev zlib1g-dev
+
+pip3 install matplotlib scipy pymemcache SciencePlots
+
+cp ../LoadGenerator/TraceReplay.cpp.template ../LoadGenerator/TraceReplay.cpp
+sed -i "/string ngnix_server_ip =/c\string ngnix_server_ip =\"$NGINX_SERVER_IP\";" ../LoadGenerator/TraceReplay.cpp
+
+cp ../LoadGenerator/run_experiment.py.template ../LoadGenerator/run_experiment.py
+sed -i "/memcached_host =/c\memcached_host = \'$MEMCACHED_SERVER_IP\'" ../LoadGenerator/run_experiment.py
+sed -i "/master_vm =/c\master_vm = \'$MASTER_VM\'" ../LoadGenerator/run_experiment.py
+
+cp ../LoadGenerator/trigger_size_k.py.template ../LoadGenerator/trigger_size_k.py
 sed -i "/memcached_host =/c\memcached_host = \'$MEMCACHED_SERVER_IP\'" ../LoadGenerator/trigger_size_k.py
+
+cp ../LoadGenerator/TraceFileGenerator.py.template ../LoadGenerator/TraceFileGenerator.py
 sed -i "/row_nums_in_db =/c\row_nums_in_db = $DB_ENTRIES" ../LoadGenerator/TraceFileGenerator.py
+
 mkdir ../LoadGenerator/traces
 mkdir ../LoadGenerator/results_warm_cache
-mkdir ../LoadGenerator/result_stats 
+mkdir ../LoadGenerator/result_stats
 mkdir ../LoadGenerator/experiment_plots
 cd ../LoadGenerator && make
-#chmod 600 ../config_files/cache_workers.pem
-#cp ../config_files/cache_workers.pem ../LoadGenerator/cache_workers.pem
 
 ssh-keyscan $NGINX_SERVER_IP  >> $HOME/.ssh/known_hosts
-ssh-keyscan $MEMCACHED_SERVER_IP  >> $HOME/.ssh/known_hosts 
+ssh-keyscan $MEMCACHED_SERVER_IP  >> $HOME/.ssh/known_hosts
+ssh-keyscan $MASTER_VM  >> $HOME/.ssh/known_hosts
+
 echo "Done executing setup_client.sh"
