c124157cc603f85dfac20088fea2c60eb4c4f4c7
Yi Chen <yichencs@umich.edu>
2024-12-06 16:28:01 -0600

    chore(leveldb-exp): minor

--- tests/LevelDB/src/tests/scripts/client/monitor_avg.py
+++ tests/LevelDB/src/tests/scripts/client/monitor_avg.py
@@ -4,6 +4,13 @@ import argparse
 import numpy as np
 
 
+parser = argparse.ArgumentParser(description="Process JSON files.")
+parser.add_argument("-f", "--filenames", nargs="+", help="The path to the JSON file(s)")
+parser.add_argument("-s", "--start", default=0, type=int, help="The start time")
+parser.add_argument("-e", "--end", default=None, type=int, help="The end time")
+
+args = parser.parse_args()
+
 def process_monitor(filename):
     print(f"[{filename}] Processing monitor data...")
     dir_name, file_name = os.path.split(filename)
@@ -42,8 +49,8 @@ def process_monitor(filename):
 
     print(f"[{filename}] Finalizing resource usage data...")
     for process_usage in process_usages.values():
-        cpu = process_usage["cpu"]
-        mem = process_usage["mem"]
+        cpu = process_usage["cpu"][args.start:args.end]
+        mem = process_usage["mem"][args.start:args.end]
         mem = [m / 1024.0 / 1024.0 if not np.isnan(m) else np.nan for m in mem]
         process_usage["cpu"] = cpu
         process_usage["mem"] = mem
@@ -52,15 +59,8 @@ def process_monitor(filename):
     for process_name in sorted(process_usages.keys()):
         if process_name != "redis-leveldb":
             ordered_process_usages[process_name] = process_usages[process_name]
-    for process_usage in ordered_process_usages.values():
-        print(f"{process_name}: avg cpu: {np.nanmean(process_usage['cpu'])}, avg mem: {np.nanmean(process_usage['mem'])}")
-
-
-parser = argparse.ArgumentParser(description="Process JSON files.")
-
-parser.add_argument("-f", "--filenames", nargs="+", help="The path to the JSON file(s)")
-
-args = parser.parse_args()
+    for process_name, process_usage in ordered_process_usages.items():
+        print(f"{process_name}[{args.start}:{args.end}]: avg cpu: {np.nanmean(process_usage['cpu'])}, avg mem: {np.nanmean(process_usage['mem'])}")
 
 cnt = len(args.filenames)
 for filename in args.filenames:
