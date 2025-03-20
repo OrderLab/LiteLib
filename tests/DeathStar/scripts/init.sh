86c9ae9dc4e3eef99689eb1a47d736bee0b23c60
Yi Chen <yichencs@umich.edu>
2025-03-20 15:51:10 -0500

    feat(DeathStar): use mine Memcached

---
+++ tests/DeathStar/scripts/init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run as root (use su or sudo)"
+    exit 1
+fi
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+pip3 aiohttp
+
+apt install lua5.1 luarocks
+luarocks install luasocket
