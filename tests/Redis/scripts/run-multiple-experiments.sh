45b5d37bbe7efc94f56327ceb9dd74fccba85618
Yi Chen <yichencs@umich.edu>
2025-03-21 16:19:23 -0500

    feat(LiteRedis-exp): minor

--- tests/Redis/scripts/run-multiple-experiments.sh
+++ tests/Redis/scripts/run-multiple-experiments.sh
@@ -4,13 +4,28 @@ set -x
 
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 
+SUFFIX=overhead
+for i in {1..10}; do
+    for MODE in vanilla replica embedded; do
+        ./run-single-experiment.sh $MODE $SUFFIX-$i 0 > $SCRIPT_DIR/logs/$MODE-experiment-script-$SUFFIX-$i.log 2>&1
+    done
+done
+
+SUFFIX=recovery
+for i in {1..3}; do
+    for MODE in vanilla replica embedded; do
+        ./run-single-experiment.sh $MODE $SUFFIX-$i 1 > $SCRIPT_DIR/logs/$MODE-experiment-script-$SUFFIX-$i.log 2>&1
+    done
+done
+
 # for SUFFIX in {1..10}; do
 #     for MODE in vanilla embedded replica; do
 #         ./run-single-experiment.sh $MODE $SUFFIX 0 > $SCRIPT_DIR/logs/$MODE-experiment-script-$SUFFIX.log 2>&1
 #     done
 # done
 
-SUFFIX=recovery
-for MODE in vanilla replica embedded; do
-    ./run-single-experiment.sh $MODE $SUFFIX 1 > $SCRIPT_DIR/logs/$MODE-experiment-script-$SUFFIX.log 2>&1
-done
+# SUFFIX=recovery
+# MODE=embedded
+# for i in {3..3}; do
+#     ./run-single-experiment.sh $MODE $SUFFIX-$i 1 > $SCRIPT_DIR/logs/$MODE-experiment-script-$SUFFIX-$i.log 2>&1
+# done
